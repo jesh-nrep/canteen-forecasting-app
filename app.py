55f0e8abfd1fef6b0a15f3a61c2a125e462f3451
Jesper Hauch <jesh@nrep.com>
2023-05-11 14:05:07 +0200

    add password

--- app.py
+++ app.py
@@ -30,30 +30,59 @@ def plot_title(start_date, end_date):
         title_str = f"Week {start_week}-{end_week}: {start_date.strftime('%d/%m')} - {end_date.strftime('%d/%m')}"
     return title_str
 
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
 def main():
-    st.title("NREP Canteen Forecasting")
-    week_start, week_end = next_week_range()
-    model = load_model("regression_model", "azure", {"container": "models"})
-    data = load_data()
-    start_date, end_date = st.date_input("Choose dates", (week_start, week_end), max_value=week_end+datetime.timedelta(weeks=1))
-    true_data = data.loc[start_date:end_date]
-    pred_data = true_data.drop(["actual"], axis=1)#.dropna()
-    #true_data['predictions'] = model.predict(fh=np.arange(1,6), X=pred_data)
-    true_data['predictions'] = model.predict(X=pred_data)
-    
-    copy_data = true_data.dropna()
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=copy_data.index.strftime("%A"), y=copy_data['actual'], name="Actual", line_color="#2ca02c"))
-    if not all(true_data['actual'] == true_data['predictions']):
-        fig.add_trace(go.Scatter(x=true_data.index.strftime("%A"), y=true_data['predictions'], name="Prediction", line_color="#1f77b4"))
-    
-    fig.update_layout(xaxis_title="Week day",
-                      yaxis_title="Number of eating guests",
-                      showlegend=True,
-                      title_x = 0.5,
-                      title_text=plot_title(start_date, end_date))
-    fig.update_yaxes(range=[0,300])
-    st.plotly_chart(fig, use_container_width=True)
+    if check_password():
+        st.title("Urban Partners Canteen Forecasting")
+        week_start, week_end = next_week_range()
+        model = load_model("regression_model", "azure", {"container": "models"})
+        data = load_data()
+        start_date, end_date = st.date_input("Choose dates", (week_start, week_end), max_value=week_end+datetime.timedelta(weeks=1))
+        true_data = data.loc[start_date:end_date]
+        pred_data = true_data.drop(["actual"], axis=1)#.dropna()
+        #true_data['predictions'] = model.predict(fh=np.arange(1,6), X=pred_data)
+        true_data['predictions'] = model.predict(X=pred_data)
+        
+        copy_data = true_data.dropna()
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=copy_data.index.strftime("%A"), y=copy_data['actual'], name="Actual", line_color="#2ca02c"))
+        if not all(true_data['actual'] == true_data['predictions']):
+            fig.add_trace(go.Scatter(x=true_data.index.strftime("%A"), y=true_data['predictions'], name="Prediction", line_color="#1f77b4"))
+        
+        fig.update_layout(xaxis_title="Week day",
+                        yaxis_title="Number of eating guests",
+                        showlegend=True,
+                        title_x = 0.5,
+                        title_text=plot_title(start_date, end_date))
+        fig.update_yaxes(range=[0,300])
+        st.plotly_chart(fig, use_container_width=True)
 
 
 if __name__ == "__main__":
