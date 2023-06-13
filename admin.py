81880940163124b2ffa996ba56fa5acd90a9c244
Mads <hvidbergm@outlook.com>
2023-06-13 10:46:07 +0200

    Add levels to ts graph

--- admin.py
+++ admin.py
@@ -1,6 +1,6 @@
 import datetime
 import streamlit as st
-import plotly.graph_objects as go
+import plotly.express as px
 def next_week_range():
     next_week = datetime.date.today() + datetime.timedelta(weeks=1)
     desired_week_start = datetime.datetime.strptime(next_week.strftime("%Y-W%W") + '-1', "%Y-W%W-%w")
@@ -28,10 +28,10 @@ def admin_app(model, data, headcount):
     true_data['predictions'] = model.predict(X=pred_data)
     
     copy_data = true_data.dropna()
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=copy_data.index.strftime("%A %d/%m"), y=copy_data['actual'], name="Actual", line_color="#2ca02c"))
+    fig = px.Figure()
+    fig.add_trace(px.Scatter(x=copy_data.index.strftime("%A %d/%m"), y=copy_data['actual'], name="Actual", line_color="#2ca02c"))
     if true_data['actual'].isna().sum() > 0:
-        fig.add_trace(go.Scatter(x=true_data.index.strftime("%A %d/%m"), y=true_data['predictions'], name="Prediction", line_color="#1f77b4"))
+        fig.add_trace(px.Scatter(x=true_data.index.strftime("%A %d/%m"), y=true_data['predictions'], name="Prediction", line_color="#1f77b4"))
     
     fig.update_layout(xaxis_title="Week day",
                     yaxis_title="Number of eating guests",
@@ -39,4 +39,8 @@ def admin_app(model, data, headcount):
                     title_x = 0.5,
                     title_text=plot_title(start_date, end_date))
     fig.update_yaxes(range=[0,headcount])
+    fig.add_hrect(y0=0, y1=90, line_width=0, fillcolor="green", opacity=0.2)
+    fig.add_hrect(y0=90, y1=180, line_width=0, fillcolor="yellow", opacity=0.2)
+    fig.add_hrect(y0=180, y1=240, line_width=0, fillcolor="black", opacity=0.2)
+    fig.add_hrect(y0=240, y1=302, line_width=0, fillcolor="red", opacity=0.2)
     st.plotly_chart(fig, use_container_width=True)
